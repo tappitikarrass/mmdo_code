74fb9dc79440121632f0417b7467f1ac3bc29b48
Andrii Lytvyn <lytvyn349@gmail.com>
2022-07-04 15:21:01 +0300

    Fix lab9

--- lab9/beta.py
+++ lab9/beta.py
@@ -1,10 +1,14 @@
 from sympy import symbols
+
 x1, x2, alpha = symbols('x1, x2, alpha')
+
 def golden(a, b, eps, f):
     C = 1.6180339887498949
+
     l = b - a
     x1 = b - l / C
     x2 = a + l / C
+
     while abs(b - a) > eps:
         if f.subs(alpha, x1) < f.subs(alpha, x2):
             b = x2
@@ -13,4 +17,5 @@ def golden(a, b, eps, f):
         l = b - a
         x1 = b - l / C
         x2 = a + l / C
+    
     return (b + a) / 2
--- lab9/main.py
+++ lab9/main.py
@@ -1,29 +1,22 @@
-from time import sleep
 from sympy import diff, symbols, Poly
 from simplex import simplex_solve
 from beta import golden
 import numpy as np
 import math
 
-x1, x2, alpha = symbols("x1 x2 alpha")
-
+x1, x2, alpha = symbols('x1 x2 alpha')
 
 def function():
-    return x1**2 + 2 * x2**2 - 3 * x1 - 8 * x2
-
+    return x1**2 - 8 * x1 - x2
 
 def gradient():
     return [diff(function(), x1), diff(function(), x2)]
 
-
 def equation(X0, Y0, b, grad_vec):
     return X0 + b * grad_vec[0], Y0 + b * grad_vec[1]
 
-
-# Finding the beta from the 7 lab (with the golden section method)
 def beta_golden(x, y, eps):
     return golden(0, 1, eps, function().subs(x1, x).subs(x2, y))
-
 
 def use_simplex(expr, A, B):
     a = Poly(expr)
@@ -37,19 +30,17 @@ def use_simplex(expr, A, B):
     res = simplex_solve(A, B, list_)
     return res.x
 
-
 def cgm(x0, eps, A, B):
     cnt = 0
     y0 = [math.inf, math.inf]
+
     while pow((pow(y0[0] - x0[0], 2) + pow(y0[1] - x0[1], 2)), 0.5) > eps:
         cnt += 1
         y0 = x0.copy()
         # 1.) Find ∇X(x)
-        grad = [
-            gradient()[0].subs([(x1, x0[0]), (x2, x0[1])]),
-            gradient()[1].subs([(x1, x0[0]), (x2, x0[1])]),
-        ]
-        print(f"iteration {cnt} :")
+        grad = [gradient()[0].subs([(x1, x0[0]), (x2, x0[1])]),
+        gradient()[1].subs([(x1, x0[0]), (x2, x0[1])])]
+        print(f"> Iteration {cnt} :")
         print(f"∇X(x{cnt - 1}) = {grad}")
         # 2.) Find x - x0
         delta_X_x0 = np.array([x1 - x0[0], x2 - x0[1]])
@@ -64,23 +55,22 @@ def cgm(x0, eps, A, B):
         beta_min = beta_golden(X, Y, eps)
         x0[0] += beta_min * h[0]
         x0[1] += beta_min * h[1]
-        print(f"X{cnt - 1} = ({2 * x0[0]}, {2 * x0[1]})")
-        print("f(x1, x2) :", 2 * function().subs(x1, x0[0]).subs(x2, x0[1]))
-        print("\n")
-        sleep(0.3)
-
+        print(f'X{cnt - 1} = ({x0[0]}, {x0[1]})')
+        print('f(x1, x2) :', function().subs(x1, x0[0]).subs(x2,
+        x0[1]))
+        print('\n')
+    
 def main():
     print("\n << The Conditional Gradient Method >> \n")
     print("∇X(x0) = ", gradient(), "\n")
-    eps = 0.001
-    # A = [[-2, -1], [2, 3]]
-    # B = [-8, 20]
-    # X0 = np.array([3.0, 2.0])
-    A = [[2, 1], [1, 5]]
+    eps = 0.01
+    # A = [[2, 1],[1, 5]]
+    # B = [16, 20]
+    # X0 = np.array([4.0, 3.0])
+    A = [[2, 1],[1, 5]]
     B = [16, 20]
     X0 = np.array([4.0, 3.0])
     cgm(X0, eps, A, B)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
